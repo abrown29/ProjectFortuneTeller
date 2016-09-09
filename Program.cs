96cb3432f1c258fa04a7957a38704d7962fc741f
Amanda Brown <amanda.brown@gmail.com>
2016-09-09 19:37:08 -0400

    Part One Complete

--- Program.cs
+++ Program.cs
@@ -12,26 +12,33 @@ namespace ProjectFortuneTeller
         {
             Console.WriteLine("What is your first name?");
             string firstName = Console.ReadLine();
+            Console.WriteLine("\n");
 
             Console.WriteLine("What is your last name?");
             string lastName = Console.ReadLine();
+            Console.WriteLine("\n");
 
             Console.WriteLine("What is yor age?");
             int userAge = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
 
             Console.WriteLine("What month were you born in? (Please enter month in numerical form 1-12)");
             int birthMonth = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
 
-            Console.WriteLine("What is your favorite ROYGBIV colour? Please enter \"Help\" to get a list of colours.");
-            string colour = Console.ReadLine();
-            if (colour.ToLower() == "help")
-                {
-                Console.WriteLine("R is for red" + "\n" + "O is for orange" + "\n" + "Y is for yellow" + "\n" + "G is for green" + "\n" + "B is for blue" + "\n" + "I is for indigo" + "\n" + "V is for violet");
-            } 
-            else
+            Console.WriteLine("What is your favorite ROYGBIV color? Please enter \"Help\" to get a list of colors.");
+            string color = Console.ReadLine();
+            if (color.ToLower() == "help")
             {
-                string colour = Console.ReadLine();
+                Console.WriteLine("R is for red" + "\n" + "O is for orange" + "\n" + "Y is for yellow" + "\n" + "G is for green" + "\n" + "B is for blue" + "\n" + "I is for indigo" + "\n" + "V is for violet");
+                Console.WriteLine("Please enter your favorite color.");
             }
+            Console.WriteLine("\n");
+
+            Console.WriteLine("How many siblings do you have?");
+            int userSibling = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+
             Console.ReadKey();
         }
     }
